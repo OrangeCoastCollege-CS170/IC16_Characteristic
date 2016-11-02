f05ccd7b50a88d174c6bec836b6a043ca74e27a5
Seve Badajoz <sevebadajoz@gmail.com>
2016-11-02 10:50:27 -0700

    completed Characteristic.java and CharacteristicDemo.java

---
+++ src/edu/orangecoastcollege/sbadajoz/ic16/Characteristic.java
@@ -0,0 +1,50 @@
+package edu.orangecoastcollege.sbadajoz.ic16;
+
+public class Characteristic {
+    private String mDescription;
+    private int mRating;
+
+    public Characteristic(String description) {
+        this.mDescription = description;
+        mRating = 0;
+    }
+
+    private boolean isValid(int newRating) {
+        return (0 <= newRating && newRating <= 10);
+    }
+
+    public void setRating(int rating) {
+        if(isValid(rating)) {
+            mRating = rating;
+        }
+    }
+
+    private void setDescription(String Description) {
+        mDescription = Description;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public int getRating() {
+        return mRating;
+    }
+
+    public double getCompatibility(Characteristic other) {
+        if (isMatch(other)) {
+            return getCompatibilityMeasure(other);
+        }
+        return 0;
+    }
+
+    private double getCompatibilityMeasure(Characteristic other) {
+        if(mRating!= 0 && other.mRating != 0)
+        return 1- Math.sqrt(mRating - other.mRating)/81;
+        return 0;
+    }
+
+    private boolean isMatch(Characteristic other) {
+        return mDescription.equals(other.mDescription);
+    }
+}
---
+++ src/edu/orangecoastcollege/sbadajoz/ic16/CharacteristicDemo.java
@@ -0,0 +1,21 @@
+package edu.orangecoastcollege.sbadajoz.ic16;
+
+public class CharacteristicDemo {
+    public static void main(String[] args) {
+        Characteristic funny1 = new Characteristic("To be able to make someone laugh");
+        Characteristic serious1 = new Characteristic("To take everything literally and stick to a strict guideline ");
+        Characteristic funny2 = new Characteristic(funny1.getDescription());
+
+        funny1.setRating(10);
+        funny2.setRating(5);
+
+        System.out.println("funny 1 has a rating of " + funny1.getRating());
+        System.out.println("funny 2 has a rating of " + funny2.getRating());
+
+        System.out.println("Funny 1 and Funny 2 have a compatibility of " + funny1.getCompatibility(funny2));
+
+        System.out.println("Funny 1 and serious 1 have a compatibility of " + funny1.getCompatibility(serious1));
+        }
+
+    }
+
